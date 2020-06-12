a6b6ef0576f3d77106fe8cda55c8e71242b7f064
Jeremy Daley <cignus@pogchamp.gg>
2020-06-12 14:01:29 -0700

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>JD INTERVIEW</h2>
+        <p>I know this wasn't required but this entire project was a learning experience for me
+            <br></br>
+            Honestly I had a great time teaching myself a lot of this.
+        </p>
+ 
+
+      </div>
+    );
+  }
+}
+ 
+export default Home;
---
+++ src/Stuff.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+ 
+class Stuff extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Branching Structure</h2>
+        <p>Here's a list of the things I've completed</p>
+        <ol>
+          <li>Set up Git Flow Branching structure on my own repo on github</li>
+        </ol>
+      </div>
+    );
+  }
+}
+ 
+export default Stuff;
